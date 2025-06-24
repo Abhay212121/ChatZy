ea9930a6f3ab9a2ebf9328f96968c64c7a86747c
Abhay <abhay.work.02@gmail.com>
2025-06-24 19:44:08 +0530

    message can be now posted!

--- src/components/BottomSection.jsx
+++ src/components/BottomSection.jsx
@@ -2,8 +2,7 @@ import { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import { Button } from "antd";
-import axios from "axios";
-import { serverAddress } from "../constants/serverAddress";
+import { socket } from "../socket";
 
 export function BottomSection() {
   const { userId } = useContext(UserContext);
@@ -14,16 +13,18 @@ export function BottomSection() {
   const handleSendMessage = async () => {
     setLoading(true);
     try {
-      const response = await axios.post(
-        `${serverAddress}/messages/postMessage`,
-        { groupId, userId, messageText }
+      socket.emit(
+        "send-message",
+        { groupId, userId, messageText },
+        (response) => {
+          if (response.status == 200) {
+            console.log("message sent!");
+            setMessageText("");
+          } else {
+            console.log("Error sending message!");
+          }
+        }
       );
-      if (response.data.status == 200) {
-        console.log("Message sent!");
-        setMessageText("");
-      } else {
-        console.log("Error encountered while sending message!");
-      }
     } catch (error) {
       console.log(error);
     } finally {
@@ -32,24 +33,28 @@ export function BottomSection() {
   };
 
   return (
-    <div className="bg-gradient-to-r to-sky-900  via-purple-800 from-purple-800 h-20 flex items-center px-12 gap-20 rounded-b-xl">
+    <div className="bg-purple-800 h-20 flex items-center px-12 gap-20 rounded-b-xl">
       <input
         type="text"
         placeholder="Enter your message"
         value={messageText}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            handleSendMessage();
+          }
+        }}
         onChange={(e) => setMessageText(e.target.value)}
         className="bg-white w-full font-body px-4 py-[0.7%] rounded-sm shadow cursor-pointer hover:shadow-md  focus:cursor-default focus:outline-0 h-10"
       />
       <Button
         onClick={handleSendMessage}
-        disabled={!messageText && loading}
+        disabled={!messageText || loading}
         loading={loading}
         style={{
-          backgroundColor: "#4070F4",
-          borderColor: "#4070F4",
+          backgroundColor: "#E12AFB",
           color: "white",
         }}
-        className="font-head1  h-10 w-20 rounded-sm cursor-pointer p-2 text-black bg-indigo-300"
+        className="!font-head1  !h-10 !w-20 !rounded-sm cursor-pointer !border-0 !p-2 !scale-115"
       >
         Send
       </Button>
